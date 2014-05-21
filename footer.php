fac3c2c6f4176e63af0efc223cec69e2553c3297
Sergio Vieira <sergiosvieira@gmail.com>
2014-05-20 21:15:48 -0300

    fix partners alignment

--- footer.php
+++ footer.php
@@ -1,11 +1,9 @@
     <!-- Footer - Begin -->
     <hr class="featurette-divider">
-
-    <div>
-        <img src="<?php echo get_template_directory_uri() . '/assets/images/patrocinadores-aerofortal.jpg'; ?>">
-    </div>
-
     <footer>
+        <div>
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/patrocinadores-aerofortal.jpg'; ?>">
+        </div>    
         <div class="container">
             <p class="pull-right"><a href="#">Back to top</a></p>
             <p>&copy; 2014 Todos os Direitos Reservados - 8 Aerofortal.</p>
